c7abf2edb8c6e3b5ca295bca3c1b2ce2193e30b5
Strahinja <43478545+keselj-strahinja@users.noreply.github.com>
2023-06-19 15:04:59 +0200

    adjusted gateway

--- gateway/main.go
+++ gateway/main.go
@@ -6,6 +6,7 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/keselj-strahinja/toll-calculator/aggregator/client"
 	"github.com/sirupsen/logrus"
@@ -39,7 +40,7 @@ func newInvoiceHandler(c client.Client) *InvoiceHandler {
 }
 
 func (h *InvoiceHandler) handleGetInvoice(w http.ResponseWriter, r *http.Request) error {
-	inv, err := h.client.GetInvoice(context.Background(), 7785114990953577266)
+	inv, err := h.client.GetInvoice(context.Background(), -1246391730)
 	if err != nil {
 		return err
 	}
@@ -56,6 +57,12 @@ func writeJSON(w http.ResponseWriter, code int, v any) error {
 
 func makeAPIFunc(fn apiFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		defer func(start time.Time) {
+			logrus.WithFields(logrus.Fields{
+				"took": time.Since(start),
+				"uri":  r.RequestURI,
+			}).Info("REQ ::")
+		}(time.Now())
 		if err := fn(w, r); err != nil {
 			writeJSON(w, http.StatusInternalServerError, map[string]string{"error": err.Error()})
 		}
